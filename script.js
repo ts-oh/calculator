26fa87901dcf2c59e7c86570434c1817f21640ec
ts-oh <tsohlove@gmail>
2021-08-12 15:13:54 +0900

    added decimal point support

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// 1. display: get number value from the keypad buttons
+// Query Selectors
 const intKeys = document.querySelectorAll('.integer');
 const operateKeys = document.querySelectorAll('.operator');
 const acKey = document.querySelector('.clear');
+const decimalKey = document.querySelector('.decimal');
 
+// Event Listners
 Array.from(intKeys).forEach(integer =>
   integer.addEventListener('click', display),
 );
-
 Array.from(operateKeys).forEach(operator =>
   operator.addEventListener('click', display),
 );
-
 acKey.addEventListener('click', allClear);
+decimalKey.addEventListener('click', display);
 
+// Clear Function
 function allClear() {
   const displayOutput = document.querySelector('.display');
   displayOutput.textContent = '';
@@ -22,7 +24,15 @@ function allClear() {
   operatorValue = '';
 }
 
-// 2. display: function to store number values into an array
+// Auto reset function
+function resetCalculation() {
+  keypadDisplay = [];
+  num1Arr = [];
+  num2Arr = [];
+  operatorValue = '';
+}
+
+// Display: function to store number values into an array
 let keypadDisplay = [];
 let num1Arr = [];
 let num2Arr = [];
@@ -52,15 +62,19 @@ function display(param) {
   } else if (operatorValue === '+' && keypadButton === '=') {
     const addition = operate(add, num1Int, num2Int);
     displayOutput.textContent = addition;
+    resetCalculation();
   } else if (operatorValue === '-' && keypadButton === '=') {
     const subtraction = operate(subtract, num1Int, num2Int);
     displayOutput.textContent = subtraction;
+    resetCalculation();
   } else if (operatorValue === '*' && keypadButton === '=') {
     const multiplication = operate(multiply, num1Int, num2Int);
     displayOutput.textContent = multiplication;
+    resetCalculation();
   } else if (operatorValue === '÷' && keypadButton === '=') {
     const division = operate(divide, num1Int, num2Int);
     displayOutput.textContent = division;
+    resetCalculation();
   } else if (operatorValue === '') {
     num1Arr.push(keypadButton);
   } else if (
@@ -86,7 +100,11 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  if (num2 === 0 || num2 === isNaN) {
+    return 'UNDEFINED: DIVIDE BY NON-ZERO NUMBER!';
+  } else {
+    return num1 / num2;
+  }
 }
 
 function operate(operator, num1, num2) {
