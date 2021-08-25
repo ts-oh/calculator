df889caf021aa3868031f8ef75c4d3a400c0fcf7
ts-oh <tsohlove@gmail>
2021-08-25 14:47:02 +0900

    new calculator logic and structure

--- script.js
+++ script.js
@@ -1,107 +1,134 @@
-// Query Selectors
-const intKeys = document.querySelectorAll('.integer');
-const operateKeys = document.querySelectorAll('.operator');
-const computeKey = document.querySelector('.compute');
-const acKey = document.querySelector('.clear');
-const decimalKey = document.querySelector('.decimal');
-const bsKey = document.querySelector('.backspace');
+let num1 = '';
+let num2 = '';
+let operator = '';
+let defaultDisplay = '0';
 
-// Event Listners
-intKeys.forEach(int => int.addEventListener('click', pushNumber));
-operateKeys.forEach(op => op.addEventListener('click', pushOperator));
-acKey.addEventListener('click', allClear);
-computeKey.addEventListener('click', handleCompute);
-decimalKey.addEventListener('click', addDecimal);
-bsKey.addEventListener('click', backspaceKey);
+const display = document.querySelector('.display');
 
-// output display numbers in to the display div
-const displayOutput = document.querySelector('.display');
+document
+  .querySelectorAll('button')
+  .forEach(element => element.addEventListener('click', handleKeyBtns));
 
-// Array to store numbers and operators
-let numArray = [];
-
-// Clear Function
-function allClear() {
-  const displayOutput = document.querySelector('.display');
-  displayOutput.textContent = '';
-  numArray = [];
-}
-
-// Reset function
-function resetCalculation() {
-  numArray = [];
-}
-
-function backspaceKey() {
-  numArray[numArray.length - 1] = numArray[numArray.length - 1].slice(0, -1);
-}
-
-function addDecimal(event) {
-  const decimal = event.target.value;
-  console.log(decimal);
-  if (numArray[numArray.length - 1].includes(decimal) === false) {
-    numArray.push(decimal);
+function handleKeyBtns(event) {
+  const inputValue = event.target.value;
+  if (isNum(inputValue)) {
+    assignNum(inputValue);
+  } else if (isBasicOperation(inputValue)) {
+    assignOperator(inputValue);
+  } else if (isDecimal(inputValue)) {
+    addDecimals(inputValue);
+  } else if (isEqual(inputValue)) {
+    calculate(inputValue);
+  } else if (isAllClearNumStorage(inputValue)) {
+    clearAllMemory();
+  } else if (isBackSpaceKey(inputValue)) {
+    deleteValue();
   }
 }
 
-function concatNum1() {
-  let joinNum = numArray.join('');
-  console.log(joinNum);
-  displayOutput.textContent = joinNum;
-  numArray = [];
-  numArray.splice(0, numArray.length, joinNum);
+// check if the inputValue (userclick) is a particular value
+function isNum(inputValue) {
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  return numbers.includes(inputValue);
 }
 
-function concatNum2() {
-  let num2Join = numArray.splice(2, numArray.length);
-  let joinNum2 = num2Join.join('');
-  console.log(joinNum2);
-  displayOutput.textContent = joinNum2;
-  numArray.splice(2, numArray.length, joinNum2);
+function isBasicOperation(inputValue) {
+  const operations = ['+', '-', '*', '/'];
+  return operations.includes(inputValue);
 }
 
-function pushNumber(event) {
-  numArray.push(event.target.value);
-  displayOutput.textContent = event.target.value;
-  if (
-    numArray[1] === '+' ||
-    numArray[1] === '-' ||
-    numArray[1] === '*' ||
-    numArray[1] === '÷'
-  ) {
-    concatNum2();
+function isEqual(inputValue) {
+  const equal = '=';
+  return equal.includes(inputValue);
+}
+
+function isDecimal(inputValue) {
+  const decimal = '.';
+  return decimal.includes(inputValue);
+}
+
+function isAllClearNumStorage(inputValue) {
+  const allClearNumStorage = 'ac';
+  return allClearNumStorage.includes(inputValue);
+}
+
+function isBackSpaceKey(inputValue) {
+  const bsKey = 'bs';
+  return bsKey.includes(inputValue);
+}
+
+// functions for backspace, all clear, decimals, pair calculation, and numbers & operators.
+
+function deleteValue() {
+  if (num1 && operator && num2) {
+    num2 = num2.slice(0, -1);
+    display.textContent = num2;
+  } else if (num1) {
+    num1 = num1.slice(0, -1);
+    display.textContent = num1;
+  }
+}
+
+function assignNum(inputValue) {
+  if (num1 && operator) {
+    num2 += inputValue;
+    display.textContent = num2;
   } else {
-    concatNum1();
-  }
-}
-function pushOperator(event) {
-  // if the last element of numArray is an operator after a pair don't push but calculate
-  numArray.push(event.target.value);
-  if (numArray.includes(event.target.value, 3)) {
-    const newNum = handleCompute();
-    numArray.push(newNum);
-    numArray.push(event.target.value);
-  } else if (
-    numArray[0] === '+' ||
-    numArray[0] === '-' ||
-    numArray[0] === '*' ||
-    numArray[0] === '+'
-  ) {
-    numArray = [];
-    return (displayOutput.textContent = 'Please enter a number!');
+    num1 += inputValue;
+    display.textContent = num1;
   }
 }
 
-function handleCompute() {
-  // when user press = split index [0] number1 , index [1] operator and index [2] number2
-  const num1 = Number(numArray[0]);
-  const operator = numArray[1].split('');
-  const num2 = Number(numArray[2]);
-  const calculation = result(operator, num1, num2);
-  const roundCalculation = parseFloat(calculation.toFixed(3));
-  displayOutput.textContent = roundCalculation;
-  resetCalculation();
-  return calculation;
+function assignOperator(inputValue) {
+  // if num2 and operator are not loaded, assign user inputValue to operator
+  if (!num2 && !operator) {
+    operator = inputValue;
+  }
+  // if user clicks operator after a pair of number calculate
+  else {
+    pairCalculation(inputValue);
+  }
+}
+
+function pairCalculation(inputValue) {
+  // if num1, operator, num2, and operator is loaded calculate result
+  // when result is returned clear num2 and set operator to user inputValue
+  if (num1 && num2 && operator) {
+    num1 = calculate().toString();
+    num2 = '';
+    operator = inputValue;
+  }
+  // if num1 and operator is loaded, assign num2 to user inputValue
+  else if (num1 && operator) {
+    num2 = inputValue;
+  }
+}
+
+function addDecimals(inputValue) {
+  const decimal = inputValue;
+  if (!num1.includes(decimal) && !operator) {
+    num1 += '.';
+    display.textContent = num1;
+  } else if (operator && !num2.includes(decimal)) {
+    num2 += '.';
+    display.textContent = num2;
+  }
+}
+
+function clearAllMemory() {
+  num1 = '';
+  num2 = '';
+  operator = '';
+  display.textContent = defaultDisplay;
+}
+
+function calculate() {
+  const convertNum1 = Number(num1);
+  const convertNum2 = Number(num2);
+  const calculate = result(operator, convertNum1, convertNum2);
+  console.log(calculate);
+  display.textContent = calculate;
+  return calculate;
 }
 
 function result(operator, num1, num2) {
